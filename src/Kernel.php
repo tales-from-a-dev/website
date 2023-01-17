61adf3e2151754dfbedad0bd5a06d6ecfd22f466
Romain Monteil <monteil.romain@gmail.com>
2023-01-17 12:51:35 +0100

    [#46] Add Symfony and Doctrine extensions for PHPStan

--- src/Kernel.php
+++ src/Kernel.php
@@ -7,8 +7,6 @@ namespace App;
 use App\Core\Doctrine\Dbal\Types\ProjectMetadataType;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
-use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class Kernel extends BaseKernel
 {
@@ -18,19 +16,6 @@ class Kernel extends BaseKernel
     {
         parent::boot();
 
-        $this->addProjectMetadataTypeToDoctrine();
-    }
-
-    private function addProjectMetadataTypeToDoctrine(): void
-    {
-        if (
-            ($serializer = $this->getContainer()->get('serializer_doctrine')) &&
-            (
-                $serializer instanceof NormalizerInterface &&
-                $serializer instanceof DenormalizerInterface
-            )
-        ) {
-            ProjectMetadataType::setSerializer($serializer);
-        }
+        ProjectMetadataType::setSerializer($this->getContainer()->get('serializer_doctrine'));
     }
 }
