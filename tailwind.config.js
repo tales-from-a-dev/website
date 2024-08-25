a087d94cd6197576c569c79a2e4c93fc1b3a7c6d
Romain Monteil <monteil.romain@gmail.com>
2024-08-26 01:07:30 +0200

    :sparkles: Replace WebpackEncore with AssetMapper (#107)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
     content: [
         "./assets/**/*.js",
         "./templates/**/*.html.twig",
-        "./node_modules/flowbite/**/*.js",
-        "./node_modules/tom-select/**/*.js",
         "./vendor/tales-from-a-dev/flowbite-bundle/templates/**/*.html.twig",
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+  plugins: [],
 }
