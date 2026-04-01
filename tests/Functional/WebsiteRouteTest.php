eaeba9f11f49c107ca82d568c710ab363ec1e751
Romain Monteil <monteil.romain@gmail.com>
2026-04-01 11:18:32 +0200

    ✨ Add Resume page

--- tests/Functional/WebsiteRouteTest.php
+++ tests/Functional/WebsiteRouteTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Tests\Functional;
 
 use App\Contact\Domain\Enum\ContactRouteNameEnum;
+use App\Resume\Domain\Enum\ResumeRouteNameEnum;
 use App\Shared\Domain\Enum\SharedRouteNameEnum;
 use App\Tests\RouteTestCase;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,6 +19,8 @@ final class WebsiteRouteTest extends RouteTestCase
     public static function urlsProvider(): iterable
     {
         yield 'GET /' => ['/', Request::METHOD_GET, SharedRouteNameEnum::WebsiteIndex];
+
+        yield 'GET /cv' => ['/cv', Request::METHOD_GET, ResumeRouteNameEnum::WebsiteResume];
 
         yield 'GET /contact' => ['/contact', Request::METHOD_GET, ContactRouteNameEnum::WebsiteContact];
         yield 'POST /contact' => ['/contact', Request::METHOD_POST, ContactRouteNameEnum::WebsiteContact];
