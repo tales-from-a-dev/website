5d099384914eef7c77b8f474de2295891cefc5e4
Romain Monteil <monteil.romain@gmail.com>
2026-06-10 17:14:53 +0200

    ⚡️ Revert font CDN with dependencies

--- assets/app.js
+++ assets/app.js
@@ -4,6 +4,7 @@
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+import '@fontsource-variable/inter/index.min.css'
 import './styles/app.css';
 
 import './stimulus_bootstrap.js';
